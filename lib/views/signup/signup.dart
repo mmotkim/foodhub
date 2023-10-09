b1e7382198568820783b941c8fccb37daf6faad3
Mmotkim <mmotkim2@gmail.com>
2023-10-09 21:54:50 +0700

    Optimized widgets

--- lib/views/signup/signup.dart
+++ lib/views/signup/signup.dart
@@ -67,83 +67,8 @@ import 'package:another_flushbar/flushbar.dart';
 //email verification ? -> REQUIRED after sign up with email/pass X (Added on after sign in + sign up with email/pass, splash screen)
 
 @RoutePage()
-class SignUpScreen extends StatefulWidget {
+class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
-
-  @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
-}
-
-class _SignUpScreenState extends State<SignUpScreen> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final nameController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<AuthController>().clearErrorMessage();
-    });
-  }
-
-  @override
-  void dispose() {
-    FormUtils.disposeAll(form);
-    super.dispose();
-  }
-
-  //input validations
-  final form = FormGroup({
-    'name': InputValidation.name,
-    'email': InputValidation.email,
-    'password': InputValidation.password,
-  });
-
-  void _handleSignUp() async {
-    final authProvider = Provider.of<AuthController>(context, listen: false);
-    final systemController =
-        Provider.of<SystemController>(context, listen: false);
-
-    if (form.valid) {
-      try {
-        //get input
-        final email = emailController.text.trim();
-        final password = passwordController.text.trim();
-        final name = nameController.text.trim();
-        print(email);
-
-        //await firebase signup
-        final result =
-            await authProvider.signUp(context, email, password, name);
-        print(result.user?.email);
-        if (result.user != null && mounted) {
-          await authProvider.sendEmailVerification(context).then((value) => {
-                context.router.push(
-                  EmailSentRoute2(email: email, isLoggedIn: true),
-                )
-              });
-        }
-
-        //clear message in case of return
-        authProvider.clearErrorMessage();
-      } on FirebaseAuthException catch (err) {
-        systemController.handleFirebaseEx(err.code);
-      } finally {
-        form.controls.forEach((key, value) {
-          value.updateValue('');
-          value.markAsUntouched();
-        });
-      }
-    } else {
-      print('form is shit');
-
-      print(form.errors);
-      form.errors.forEach((key, value) {
-        print('$key $value');
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -154,12 +79,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
       child: Scaffold(
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-          child: ReactiveForm(
-            formGroup: form,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: signUpContent(context),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: signUpContent(context),
           ),
         ),
       ),
@@ -178,38 +100,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
       errorMessage(context),
       const SizedBox(height: 10.0),
       // Full name field
-      BigField(
-        hintText: 'nameGuide'.tr(),
-        obscureText: false, //obscureText
-        textInputType: TextInputType.text,
-        label: 'fullName'.tr(),
-        formName: 'name',
-        controller: nameController,
-        validationMessages: InputValidation.nameMap,
-      ),
-      const SizedBox(height: 18.0), // Spacing
-      // Email Field
-      BigField.email(
-        formName: 'email',
-        controller: emailController,
-        label: 'email'.tr(),
-      ),
-      // Password Field
-      const SizedBox(height: 18.0), // Spacing
-      BigField.password(
-        hintText: 'passGuide'.tr(),
-        formName: 'password',
-        controller: passwordController,
-        label: 'pass'.tr(),
-      ), // Sign-Up Button
-      const SizedBox(height: 33.0), // Spacing
-
-      Center(
-        child: FormSubmitButton(
-          text: 'signUp'.tr().toUpperCase(),
-          onPressed: _handleSignUp,
-        ),
-      ),
+      const SignUpForm(),
 
       //already have an account?
       const SizedBox(height: 20.0), // Spacing
@@ -229,7 +120,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
       // Sign in with Text and Vertical Lines
       // HorizontalSeparator.dark(text: 'signUpWith'.tr()),
       // const SizedBox(height: 15.0), // Spacing
-
       // Padding(
       //   padding: const EdgeInsets.symmetric(horizontal: 26.0),
       //   child: Row(
@@ -313,6 +203,129 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Center(
       child: Text(context.watch<AuthController>().errorMessage ?? '',
           style: CustomTextStyle.errorText(context)),
+    );
+  }
+}
+
+//Stateful form
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AuthController>().clearErrorMessage();
+    });
+  }
+
+  @override
+  void dispose() {
+    FormUtils.disposeAll(form);
+    super.dispose();
+  }
+
+  //input validations
+  final form = FormGroup({
+    'name': InputValidation.name,
+    'email': InputValidation.email,
+    'password': InputValidation.password,
+  });
+
+  void _handleSignUp() async {
+    final authProvider = Provider.of<AuthController>(context, listen: false);
+    final systemController =
+        Provider.of<SystemController>(context, listen: false);
+
+    if (form.valid) {
+      try {
+        //get input
+        final email = emailController.text.trim();
+        final password = passwordController.text.trim();
+        final name = nameController.text.trim();
+        print(email);
+
+        //await firebase signup
+        final result =
+            await authProvider.signUp(context, email, password, name);
+        print(result.user?.email);
+        if (result.user != null && mounted) {
+          await authProvider.sendEmailVerification(context).then((value) => {
+                context.router.push(
+                  EmailSentRoute2(email: email, isLoggedIn: true),
+                )
+              });
+        }
+
+        //clear message in case of return
+        authProvider.clearErrorMessage();
+      } on FirebaseAuthException catch (err) {
+        systemController.handleFirebaseEx(err.code);
+      } finally {
+        form.controls.forEach((key, value) {
+          value.updateValue('');
+          value.markAsUntouched();
+        });
+      }
+    } else {
+      print('form is shit');
+
+      print(form.errors);
+      form.errors.forEach((key, value) {
+        print('$key $value');
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ReactiveForm(
+      formGroup: form,
+      child: Column(
+        children: [
+          BigField(
+            hintText: 'nameGuide'.tr(),
+            obscureText: false, //obscureText
+            textInputType: TextInputType.text,
+            label: 'fullName'.tr(),
+            formName: 'name',
+            controller: nameController,
+            validationMessages: InputValidation.nameMap,
+          ),
+          const SizedBox(height: 18.0), // Spacing
+          // Email Field
+          BigField.email(
+            formName: 'email',
+            controller: emailController,
+            label: 'email'.tr(),
+          ),
+          // Password Field
+          const SizedBox(height: 18.0), // Spacing
+          BigField.password(
+            hintText: 'passGuide'.tr(),
+            formName: 'password',
+            controller: passwordController,
+            label: 'pass'.tr(),
+          ), // Sign-Up Button
+          const SizedBox(height: 33.0), // Spacing
+
+          Center(
+            child: FormSubmitButton(
+              text: 'signUp'.tr().toUpperCase(),
+              onPressed: _handleSignUp,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
